a43d4d160d31acffb3ba86c7ccf91cfb44e6861b
jing <shajingyuan@me.com>
2025-04-16 11:32:54 +0200

    add adaptive vit model

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ ViT parameter reduction via Gestalt Principles
 ---
 ## Docker
 ``` 
-docker build -t gestalt_vit:latest .
+docker build -t gvit:latest .
 
 docker run -it --gpus all -v /home/ml-jsha/gestalt_vit:/app --rm gvit:latest
+docker run -it --gpus all  --shm-size=8g  -v /home/ml-jsha/gestalt_vit:/app  -v /home/ml-jsha/.netrc:/root/.netrc  -v /home/ml-jsha/storage/tiny-imagenet-200:/tiny-224 --rm --name vit-tiny-train gvit 
+
 ```
 
 ---
--- timm_train/run_baseline.sh
+++ timm_train/run_baseline.sh
@@ -19,3 +19,21 @@ CUDA_VISIBLE_DEVICES=6 python -m train /tiny-224 \
   --log-wandb \
   --experiment vit-tiny-baseline-tiny224 \
   --output output/vit-tiny-baseline-tiny224
+
+
+
+CUDA_VISIBLE_DEVICES=6 python -m train /tiny-224 \
+  --model adaptive_vit_tiny_patch16_224 \
+  --input-size 3 224 224 \
+  --epochs 200 \
+  --batch-size 128 \
+  --workers 2 \
+  --lr 5e-4 \
+  --opt adamw \
+  --weight-decay 0.05 \
+  --sched cosine \
+  --warmup-epochs 5 \
+  --amp \
+  --log-wandb \
+  --experiment vit-tiny-baseline-tiny224 \
+  --output output/adaptive_vit_tiny_patch16_224
